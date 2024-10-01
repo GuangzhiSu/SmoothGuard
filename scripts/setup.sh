50b840fcbfeb89be6b52052610ef50223a8ea29a
Guangzhi Su <163370210+GuangzhiSu@users.noreply.github.com>
2024-10-01 16:40:02 -0400

    Add files via upload

---
+++ scripts/setup.sh
@@ -0,0 +1,37 @@
+nohup deepspeed --include localhost:0 llava/train/train_mem.py \
+   --lora_enable True --lora_r 128 --lora_alpha 256 --mm_projector_lr 2e-5 \
+   --deepspeed ./scripts/zero3.json \
+   --model_name_or_path liuhaotian/llava-v1.5-7b \
+   --version v1 \
+   --data_path /home/gs285/LLaVA/llava_v1_5_mix665k.json \
+   --image_folder ./playground/data \
+   --vision_tower openai/clip-vit-large-patch14-336 \
+   --mm_projector_type mlp2x_gelu \
+   --mm_vision_select_layer -2 \
+   --mm_use_im_start_end False \
+   --mm_use_im_patch_token False \
+   --image_aspect_ratio pad \
+   --group_by_modality_length True \
+   --bf16 True \
+   --output_dir ./checkpoints/llava_1.5_gamma_0.6_temp_0.05_std_0.07 \
+   --num_train_epochs 1 \
+   --per_device_train_batch_size 4 \
+   --per_device_eval_batch_size 4 \
+   --gradient_accumulation_steps 16 \
+   --evaluation_strategy "no" \
+   --save_strategy "steps" \
+   --save_steps 50000 \
+   --save_total_limit 1 \
+   --learning_rate 2e-5 \
+   --weight_decay 0. \
+   --warmup_ratio 0.03 \
+   --lr_scheduler_type "cosine" \
+   --logging_steps 1 \
+   --tf32 True \
+   --model_max_length 2048 \
+   --gradient_checkpointing True \
+   --dataloader_num_workers 4 \
+   --lazy_preprocess True \
+   --report_to wandb \
+   --freeze_mm_mlp_adapter True > llava_1.5_gamma_0.5_temp_0.05.log 2>&1 &
+   #--pretrain_mm_mlp_adapter ./checkpoints/llava-v1.5-7b-pretrain/mm_projector.bin \
